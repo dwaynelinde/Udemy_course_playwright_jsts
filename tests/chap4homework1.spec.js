7cc4d8c5c0f4c87fc18e7d422b98beaf0b4726df
Dwayne Linde <lindedwayne@gmail.com>
2026-05-11 18:07:35 -0700

    chap 4 homework tests

---
+++ tests/chap4homework1.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test'); 
+
+// constyvariables to use in tests
+
+
+// tests 
+
+// test1 
+
+// going to website
+
+// making a new account
+
+// logging in with that new account's username and password
+
+// list items on login page, and return results to user
+
+
+
