770dec08754e30b53962ac503235ccc061322393
Dwayne Linde <lindedwayne@gmail.com>
2026-05-15 21:25:53 -0700

    added values to input

--- tests/chap4homework1.spec.js
+++ tests/chap4homework1.spec.js
@@ -9,43 +9,52 @@ const { test, expect } = require('@playwright/test');
 test("Browser Chapter 4 Homework", async ({browser})=>{
 
 
-// const variables to use in tests
+    // const variables to use in tests
 
-// Initialize the browser and page first. 
-// Login Page: website: https://rahulshettyacademy.com/client/#/auth/login
-// Register Page: website: https://rahulshettyacademy.com/client/#/auth/register
-// page: https://rahulshettyacademy.com/client/#/auth/
+    // Initialize the browser and page first. 
+    // Login Page: website: https://rahulshettyacademy.com/client/#/auth/login
+    // Register Page: website: https://rahulshettyacademy.com/client/#/auth/register
+    // page: https://rahulshettyacademy.com/client/#/auth/
 
-const context = await browser.newContext(); 
-const pageRegister = await browser.newPage(); 
-const pageLogin = await browser.newPage(); 
+    const context = await browser.newContext(); 
+    const pageRegister = await browser.newPage(); 
+    const pageLogin = await browser.newPage(); 
 
-// Now, consts for form variables. 
+    // Now, consts for form variables. 
 
-const firstName = pageRegister.locator('#firstName'); 
-const lastName = pageRegister.locator('#lastName'); 
-const email = pageRegister.locator('#userEmail'); 
-const phoneNumber = pageRegister.locator('#userMobile'); 
+    const firstName = pageRegister.locator('#firstName'); 
+    const lastName = pageRegister.locator('#lastName'); 
+    const email = pageRegister.locator('#userEmail'); 
+    const phoneNumber = pageRegister.locator('#userMobile'); 
 
-// error: 
-// const occupation = pageRegister.locator(getByRole('combobox')); 
+    // error: 
+    // const occupation = pageRegister.locator(getByRole('combobox')); 
 
-// error:
-// const gender = pageRegister.locator(getByText('Gender Male Female')); 
+    // error:
+    // const gender = pageRegister.locator(getByText('Gender Male Female')); 
 
-const password = pageRegister.locator('#userPassword'); 
-const confirmPassword = pageRegister.locator('#confirmPassword')
+    const password = pageRegister.locator('#userPassword'); 
+    const confirmPassword = pageRegister.locator('#confirmPassword')
 
-// error:
-// const eighteenOrOlder = pageRegister.locator(getByRole('checkbox')); 
-
-
-// going to website: https://rahulshettyacademy.com/client/#/auth/register
-// what fields are required: 
+    // error:
+    // const eighteenOrOlder = pageRegister.locator(getByRole('checkbox')); 
 
 
-await pageRegister.goto("https://rahulshettyacademy.com/client/#/auth/register");
-console.log(await pageRegister.title());
+    // going to website: https://rahulshettyacademy.com/client/#/auth/register
+    // what fields are required: 
+
+
+    await pageRegister.goto("https://rahulshettyacademy.com/client/#/auth/register");
+    console.log(await pageRegister.title());
+
+    // values for the fields
+
+    await firstName.fill("testfirst1"); 
+    await lastName.fill("testlast1"); 
+    await email.fill("test@example.org");
+    await phoneNumber.fill("1-212-555-1212"); 
+    await password.fill("testpass1!"); 
+    await confirmPassword.fill("testpass1!"); 
 
 
 });  
