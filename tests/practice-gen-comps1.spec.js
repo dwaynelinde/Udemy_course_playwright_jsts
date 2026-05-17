42f4734035086b1bbc12430e7be86e91266aa6ad
Dwayne Linde <lindedwayne@gmail.com>
2026-05-16 18:25:54 -0700

    constants added to page

--- tests/practice-gen-comps1.spec.js
+++ tests/practice-gen-comps1.spec.js
@@ -4,7 +4,18 @@ const { test, expect } = require('@playwright/test');
 test("Practice General Elements on a Website", async ({browser})=>
 {
 
+    // Constants to initialize first: 
+    const context = await browser.newContext(); 
+    const page = await context.newPage(); 
 
+
+    // Get the page!
+    await page.goto("https://commitquality.com/practice-general-components"); 
+    
+    // Get the title, to make sure it matches the correct page.
+    console.log(await page.title()); 
+
+    // Now, try to click the form. 
 
 
 }); 
