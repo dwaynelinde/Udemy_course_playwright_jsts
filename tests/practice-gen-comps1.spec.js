1f53150f435d8fb4247d38326cc7009f01426aef
Dwayne Linde <lindedwayne@gmail.com>
2026-05-19 14:42:20 -0700

    updated homework

--- tests/practice-gen-comps1.spec.js
+++ tests/practice-gen-comps1.spec.js
@@ -17,10 +17,9 @@ test("Practice General Elements on a Website", async ({browser})=>
 
     // Now, try to click the form. 
     
-    // await page.getByLabel('Checkbox 1').check();
-
+    // THIS WORKS!  
     expect(page.getByLabel('Checkbox 1')).toBeChecked;
-    
+
 
 
 
