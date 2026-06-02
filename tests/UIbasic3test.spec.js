15f8a2135036eac420aa0446b318e3460fd94101
Dwayne Linde <lindedwayne@gmail.com>
2026-06-01 20:22:54 -0700

    code addition

--- tests/UIbasic3test.spec.js
+++ tests/UIbasic3test.spec.js
@@ -166,12 +166,12 @@ test.only("Dynamically finding product", async ({ page })=>
     for(let i = 0; i < count; ++i)
     {
          if (await products.nth(i).locator("b").textContent() === productName)
-            {
-                // Add product to cart. 
-                await products.nth(i).locator("text= Add To Cart").click(); 
-                break;     
+        {
+            // Add product to cart. 
+            await products.nth(i).locator("text= Add To Cart").click(); 
+            break;     
 
-            }
+        }
     }
 
     await page.locator("[routerlink*='cart']").click(); 
@@ -207,6 +207,21 @@ test.only("Dynamically finding product", async ({ page })=>
     await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. "); 
     const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent(); 
     console.log(orderId); 
+
+
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+
+    for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
 }); 
 
