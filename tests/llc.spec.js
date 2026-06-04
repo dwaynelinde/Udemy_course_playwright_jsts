fd36cbe3f3b029421636cf6dc41e90e78099d322
Dwayne Linde <lindedwayne@gmail.com>
2026-06-03 17:48:16 -0700

    added to labels app

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -2,8 +2,17 @@ import { test, expect } from '@playwright/test';
 
 
 // Start of Section 7! 
+
+// test website: https://rahulshettyacademy.com/angularpractice/
+
 test('Playwright Special Locators', async ({ page}) => {
 
+    await page.goto("https://rahulshettyacademy.com/angularpractice/"); 
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel("Employed").check(); 
+    await page.getByLabel("Gender").selectOption("Female"); 
+
+    
 }); 
 
 
