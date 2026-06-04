73a3fc1f8eb515e9dbdf328c61e830da7544ff43
Dwayne Linde <lindedwayne@gmail.com>
2026-06-03 18:09:56 -0700

    button

--- tests/llc.spec.js
+++ tests/llc.spec.js
@@ -11,8 +11,12 @@ test('Playwright Special Locators', async ({ page}) => {
     await page.getByLabel("Check me out if you Love IceCreams!").click();
     await page.getByLabel("Employed").check(); 
     await page.getByLabel("Gender").selectOption("Female"); 
+    await page.getByPlaceholder("Password").fill("abc123"); 
+    await page.getByRole("button", { name: 'Submit'}).click(); 
 
-    
+
+    // start of Lesson 39. 
+
 }); 
 
 
