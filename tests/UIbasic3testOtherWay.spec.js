b111c8b4db75ccc9e0554da4ae72fbb00a3dc77c
Dwayne Linde <lindedwayne@gmail.com>
2026-06-07 13:33:26 -0700

    start of refactoring code

--- tests/UIbasic3testOtherWay.spec.js
+++ tests/UIbasic3testOtherWay.spec.js
@@ -3,12 +3,17 @@ const { text } = require('stream/consumers');
 
 // Avoiding CSS Selectors. 
 
-test("xxx", async ({browser})=>
+test("Webst Client App Login From Lesson 43 On", async ({ page })=>
 {
-
     // Avoiding CSS Selectors
-
     // type in code from lesson 43, after art walk. 
+    // Art Walk ruled. Now, on to coding. 
+
+    const email = "anshika@gmail.com"; 
+    const productName = 'ZARA COAT 3'; 
+    const products = page.locator(".card-body");     
+
+
 
 
 
