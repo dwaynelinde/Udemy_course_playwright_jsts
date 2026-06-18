b10e7a22290864a9e847c63e95ac43b59e889d18
Dwayne Linde <lindedwayne@gmail.com>
2026-06-17 22:41:13 -0700

    statejson

--- tests/WebAPIPart2.spec.js
+++ tests/WebAPIPart2.spec.js
@@ -1,16 +1,20 @@
 
 
-const {test, expect, request} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 
-test.beforeAll(async()=> 
+test.beforeAll(async({browser})=> 
 {
-
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    
     await page.goto("https://rahulshettyacademy.com/client"); 
     await page.locator("#userEmail").fill("anshika@gmail.com");
     await page.locator("#userPassword").fill("Iamking@000");
-    await page.locator("button[type='submit']").click();
+    await page.locator("[value='Login']").click();
     await page.waitForLoadState('networkidle');
+
+    await context.storageState({path:'state.json'});
 
 })
 
