f8a087e7d14e3e84713f9a87b57f7ad8e46cb737
Dwayne Linde <lindedwayne@gmail.com>
2026-06-30 15:29:07 -0700

    screenshot tests

--- tests/morevalidations.spec.js
+++ tests/morevalidations.spec.js
@@ -38,5 +38,24 @@ test("Popup validations", async({page}) =>
 
     // end of lesson 52
 
+})
+
+
+test.only("Screenshot and visual comparison", async({page}) => 
+{
+
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/"); 
+    await expect(page.locator('#displayed-text')).toBeVisible(); 
+    
+    // partial screenshot
+    await page.locator('#displayed-text').screenshot({path: 'partialscreen.png'}); 
+    
+    await page.locator('#hide-textbox').click(); 
+
+    // full page screenshot
+
+    await page.screenshot({path: 'screenshottest1.png'});  
+
+    await expect(page.locator("#displayed-text")).toBeHidden(); 
 
 })
