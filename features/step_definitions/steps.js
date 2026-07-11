b017aae135c98a0a17bcd20d52128df318f29fed
Dwayne Linde <lindedwayne@gmail.com>
2026-07-11 00:06:26 -0700

    cucumber some more

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,7 +1,7 @@
 // const assert = require('assert')
 const { When, Then, Given } = require('@cucumber/cucumber')
 // const { Greeter } = require('../../src')
-const {POManager} = require('../pageobjects/POManager'); 
+const {POManager} = require('../../pageobjects/POManager'); 
 const {test,expect, playwright} = require('@playwright/test');
 
 
@@ -20,22 +20,35 @@ Given('a login to Ecommerce application with {string} and {string}', async funct
 });
 
 
-When('I add {string} to Cart', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+When('I add {string} to Cart', async function (string) 
+{
+    const dashboardPage = poManager.getDashboardPage(); 
+    await dashboardPage.searchProductAddCart(data.productName); 
+    await dashboardPage.navigateToCart();  
 });
 
-Then('Verify {string} is displayed in the Cart', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('Verify {string} is displayed in the Cart', async function (string) 
+{
+    const cartPage = poManager.getCartPage(); 
+    await cartPage.VerifyProductDisplayed(data.productName); 
+    await cartPage.Checkout(); 
 });
 
-When('Enter valid details and place the order', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+When('Enter valid details and place the order', async function () 
+{
+    const ordersReviewPage = poManager.getOrdersReviewPage(); 
+    await ordersReviewPage.searchCountryAndSelect("ind","India"); 
+    const orderId = await ordersReviewPage.SubmitAndGetOrderId(); 
+    console.log(orderId); 
+
 });
 
-Then('Verify order is present in the Order History', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then('Verify order is present in the Order History', async function () 
+{
+    await dashboardPage.navigateToOrders(); 
+    const ordersHistoryPage = poManager.getOrdersHistoryPage(); 
+    await orderHistoryPage.searchOrderAndSelect(orderId); 
+    expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy(); 
+
+
 });
